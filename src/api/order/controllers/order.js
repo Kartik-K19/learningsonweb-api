095e82919f012f89cebe211c8b9a17c431380dc8
Kartik <kartiklonkar@gmail.com>
2023-06-01 19:31:43 +0530

    update

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -28,6 +28,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
           };
         })
       );
+      
 
       const session = await stripe.checkout.sessions.create({
         shipping_address_collection: { allowed_countries: ["IN"] },
